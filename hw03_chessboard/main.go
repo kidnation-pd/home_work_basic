fa5ac8ca397d0fc310cb6d0f4e4290bd9f8dee0e
Kidnation <kidnation@mail.ru>
2024-04-24 21:25:23 +0300

    completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Place your code here.
+	var h, v int
+	fmt.Print("Введите строки столбцы > ")
+	_, err := fmt.Scan(&h, &v)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < v; j++ {
+			if (i+j)%2 == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }
